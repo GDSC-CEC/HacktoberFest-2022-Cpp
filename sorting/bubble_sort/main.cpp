d0fd8bb1eae806d636a96f22b55e5a3aa0ba0390
webdemo07 <98474970+webdemo07@users.noreply.github.com>
2022-10-09 21:39:11 +0530

    Update main.cpp

--- sorting/bubble_sort/main.cpp
+++ sorting/bubble_sort/main.cpp
@@ -6,6 +6,17 @@ using namespace std;
 void bubbleSort(int arr[],int size)
 {  
          // enter your code here
+       int i, j;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
 }
 
 
