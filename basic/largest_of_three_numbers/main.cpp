ec9c0f00e8b546347e889a95cd8fc7bcce318486
Shubham Bhardwaj <sbmbhardwaj01@gmail.com>
2022-10-09 21:54:51 +0530

    task completed

--- basic/largest_of_three_numbers/main.cpp
+++ basic/largest_of_three_numbers/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
  
 int largest(int a,int b,int c){
    //Enter your code here
+   if(a>b) {
+       if(a>c) cout<<a<<endl;
+       else  cout<<c<<endl;
+   }
+   else if(b>c) cout<<b<<endl;
+   else cout<<c<<endl;
 }
 
 //Driver Code
