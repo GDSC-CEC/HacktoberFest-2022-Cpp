5d166290b029b9649123e78811c3ec350ccc479f
Sanyam <sanyam.makhija17@gmail.com>
2022-10-09 19:03:10 +0530

    Update main.cpp
    Now this program is giving fibonacci series.

--- loops/fibonacci_iterative/main.cpp
+++ loops/fibonacci_iterative/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int fibonacci(int n){
    //Enter your code here
    // print the answer in the fucntion itself
+   if(n<2){
+    return 1;
+   }
+ return fibonacci(n-2)+fibonacci(n-1);
+ 
 }
 
 //Driver Code
