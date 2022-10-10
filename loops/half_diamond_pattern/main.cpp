2740f0914ea779f8522fd22808a60800e4c996de
Ankit Shubham <111640642+AnkitShubham@users.noreply.github.com>
2022-10-10 18:29:29 +0530

    Updated the function to print half diamond pattern
    Used if else ladder and nested for loop in the function to print the half diamond pattern of height n.

--- loops/half_diamond_pattern/main.cpp
+++ loops/half_diamond_pattern/main.cpp
@@ -2,7 +2,44 @@
 using namespace std;
  
 void half_diamond_pattern(int n){
-   //Enter your code here
+    int i,j;
+    
+   if (n%2==0)
+   {
+       cout<<"Invalid Number";
+   }
+   
+   else if(n==1)
+   {
+       cout<<"Invalid Number";
+   }
+   
+   else
+   {
+       for(i=1;i<=(n/2)+1;i++)
+       {
+           for(j=1;j<=(n/2)+1;j++)
+           {
+               if(j<=i)
+               {
+                   cout<<"*";
+               }
+           }
+           cout<<endl;
+       }
+       
+       for(i=1;i<=(n/2)+1;i++)
+       {
+           for(j=1;j<=(n/2)+1;j++)
+           {
+               if(j>i)
+               {
+                   cout<<"*";
+               }
+           }
+           cout<<endl;
+       }
+   }
 }
 int main()
 {   
