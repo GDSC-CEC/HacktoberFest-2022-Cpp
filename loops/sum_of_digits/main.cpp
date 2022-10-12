855e5a5f9bd641a134f840dc51a7a22f8e29ec8b
vijay kumar <kumarvvijay059@gmail.com>
2022-10-12 21:59:27 +0530

    Sum of digits file addded

--- loops/sum_of_digits/main.cpp
+++ loops/sum_of_digits/main.cpp
@@ -2,7 +2,25 @@
 using namespace std;
  
 double sum_of_digits(int n){
-   //Enter your code here
+    if (n>0)
+    {
+        int r , sum =0;
+        while (n>0)
+        {
+            r = n % 10;
+            n = n / 10;
+            sum = sum + r;
+        }
+   
+        return sum;
+    }
+    
+    
+    else
+    {
+        return -1;
+    }
+    
 }
 
 //Driver Code
