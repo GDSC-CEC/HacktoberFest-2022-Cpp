220fd2b4d434245ca3aaf1f3829023ab5ab3ed13
Arshdeep Singh <arshhanjra55@gmail.com>
2022-10-12 23:07:31 +0530

    Insertion Sort Solution

--- sorting/insertion_sort/main.cpp
+++ sorting/insertion_sort/main.cpp
@@ -2,12 +2,22 @@
 #include <iostream>
 using namespace std;
  
-
-void insertionSort(int arr[],int size)
-{  
-         // enter your code here
-}
-
+void insertionSort(int arr[], int n) 
+{ 
+    int i, key, j; 
+    for (i = 1; i < n; i++)
+    { 
+        key = arr[i]; 
+        j = i - 1; 
+  
+        while (j >= 0 && arr[j] > key)
+        { 
+            arr[j + 1] = arr[j]; 
+            j = j - 1; 
+        } 
+        arr[j + 1] = key; 
+    } 
+} 
 
 void printArray(int arr[], int n)  // function to print elements of array
 {
