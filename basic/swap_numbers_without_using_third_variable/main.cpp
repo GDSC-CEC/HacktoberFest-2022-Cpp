d5e04fa9962a4e732d25a1d7f5a5ff4573d4f1ba
vijay kumar <kumarvvijay059@gmail.com>
2022-10-12 20:19:26 +0530

    Swapping without variable code file modified

--- basic/swap_numbers_without_using_third_variable/main.cpp
+++ basic/swap_numbers_without_using_third_variable/main.cpp
@@ -2,8 +2,11 @@
 #include<iostream>
 using namespace std;
  
-void swap(int &a,int &b){
-   //Enter your code here
+void swap(int *a,int *b){
+    
+    *a=*a+*b;
+    *b=*a-*b;
+    *a=*a-*b;
 }
 
 //Driver Code
@@ -11,7 +14,7 @@ int main()
 {   
     int a,b;
     cin>>a>>b;
-    swap(a,b);
+    swap(&a,&b);
     cout<<a<<" "<<b;
     return 0;
 }
