797e421b3357180c71bcf6d4c0f29a89d8319124
Indradeo <indradeokumar614idk@gmail.com>
2022-10-12 15:11:55 +0530

    linear_search_recursion modified

--- searching/linear_search_recursion/main.cpp
+++ searching/linear_search_recursion/main.cpp
@@ -5,7 +5,14 @@ using namespace std;
 
 int linearSearch_recursive(int arr[],int size ,int element)
 {  
-    // enter your code here
+    if(size == 0)
+        return -1;
+    if(arr[0] == element)
+        return 0;
+    int ans = linearSearch_recursive(arr+1, size-1, element);
+    if(ans == -1)
+        return -1;
+    return ans + 1;
 }
  
 // Driver Code
