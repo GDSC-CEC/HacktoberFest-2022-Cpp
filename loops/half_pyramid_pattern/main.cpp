5044080f48725e345e05d7564136ed1750bc1382
harini4444 <harinimanche4444@gmail.com>
2022-10-13 12:29:52 +0530

    added file

--- loops/half_pyramid_pattern/main.cpp
+++ loops/half_pyramid_pattern/main.cpp
@@ -3,6 +3,23 @@ using namespace std;
  
 void half_pyramid_pattern(int n){
    //Enter your code here
+   if(n>0){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+               if(j<=i){
+                cout<<"*";
+               }
+               else{
+                cout<<" ";
+               }
+            }
+            cout<<"\n";
+        }
+   }
+   else{
+     cout<<"enter the valid n";
+   }
+   
 }
 int main()
 {   
