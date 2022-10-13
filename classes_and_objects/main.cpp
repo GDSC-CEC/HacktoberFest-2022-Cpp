ec6cb8a00ab2b0ab90a3c5ec5cd960db95098bb4
Cleansword <sagarjar213@gmail.com>
2022-10-13 18:03:02 +0530

    added code

--- classes_and_objects/main.cpp
+++ classes_and_objects/main.cpp
@@ -4,11 +4,29 @@ using namespace std;
  
 class Sample{
     // define your class here
+    int a, b;
+    public:
+    void data(int c, int d){
+        a=c;
+        b=d;
+        cout<<"Numbers after swapping -> "<<a <<" "<<b<<endl;
+    }
+    void swap(){
+        int t;
+        t=a;
+        a=b;
+        b=t;
+        cout<<"Numbers before swapping -> "<<a <<" "<<b<<endl;
+    }
+
 };
 
 
 int main()
 {   
      // implement the use of objects here
+     Sample s;
+     s.data(4,5);
+     s.swap();
     return 0;
 }
