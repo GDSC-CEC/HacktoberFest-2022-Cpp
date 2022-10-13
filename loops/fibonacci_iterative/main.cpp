80b1f30bc935b4991a77dfb48c7d763e141bdabb
Shubham Bhardwaj <sbmbhardwaj01@gmail.com>
2022-10-13 17:46:50 +0530

    Solution for Fibonacci iterative

--- loops/fibonacci_iterative/main.cpp
+++ loops/fibonacci_iterative/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int fibonacci(int n){
    //Enter your code here
    // print the answer in the fucntion itself
+   int a=0,b=1,c;
+   for(int i=0;i<n;i++){
+       c=a+b;
+       a=b;
+       b=c;
+   }
+   return a;
+
 }
 
 //Driver Code
