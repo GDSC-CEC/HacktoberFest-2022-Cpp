e090b390b4ba426f59b1237d9199d8057acdfc67
Shubham Bhardwaj <sbmbhardwaj01@gmail.com>
2022-10-13 17:53:03 +0530

    Solution for sum of digits

--- loops/sum_of_digits/main.cpp
+++ loops/sum_of_digits/main.cpp
@@ -3,6 +3,16 @@ using namespace std;
  
 double sum_of_digits(int n){
    //Enter your code here
+
+   double res=0;
+   if(n>0){
+   while(n>0){
+       res+=n%10 ;
+       n/=10;
+   }
+   return res;
+   }
+   return -1;
 }
 
 //Driver Code
