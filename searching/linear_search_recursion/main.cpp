2677fec9b79a1bc769d6af66fbcef0cfcda8bf65
Dev kumar <83683369+Dev092@users.noreply.github.com>
2022-10-14 09:28:43 +0530

    Update main.cpp

--- searching/linear_search_recursion/main.cpp
+++ searching/linear_search_recursion/main.cpp
@@ -1,28 +1,41 @@
 // C++ program to implement Linear Search using recursion
 #include <iostream>
 using namespace std;
- 
+
 
 int linearSearch_recursive(int arr[],int size ,int element)
-{  
+{
     // enter your code here
+    size--;
+
+
+    if (size < 0) {
+        return -1;
+    }
+
+    if (arr[size] == element) {
+        return size;
+    }
+
+
+    return linearSearch_recursive(arr, size, element);
 }
- 
+
 // Driver Code
 int main()
-{   
+{
     int size;
     cin>>size; //size of array
     int arr[size];
-    
+
     for(int i=0;i<size;i++){
        cin>>arr[i]; // elemnents in the array
     }
-   
+
    int element; // element to be searched
     cin>>element;
- 
-    int result = linearSearch__recursive(arr,size,element);
+
+    int result = linearSearch_recursive(arr,size,element);
     (result == -1)
         ? cout << "Element is not present in array"
         : cout << "Element is present at index " << result;
