7430fbc9a6ef4f4aba5dccad4dcaa19f2387b2df
Abhishek Kumar <abhishek02102003@gmail.com>
2022-10-14 23:49:30 +0530

    updated file sum_of_digits/main.cpp

--- loops/sum_of_digits/main.cpp
+++ loops/sum_of_digits/main.cpp
@@ -3,6 +3,17 @@ using namespace std;
  
 double sum_of_digits(int n){
    //Enter your code here
+    double sum=0, rem=0; 
+    if(n>=0){
+        while(n>0){
+            rem = n % 10;
+            sum = sum + rem;
+            n = n / 10;
+        }
+    }else if(n<0){
+        return sum = -1;
+    }
+    return sum;
 }
 
 //Driver Code
