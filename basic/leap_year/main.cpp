8aa93514830e94e117e34042a029cc402aff405c
Manjesh M <98503314+manjesh2022@users.noreply.github.com>
2022-10-21 21:57:14 +0530

    Update with less line of Code

--- basic/leap_year/main.cpp
+++ basic/leap_year/main.cpp
@@ -1,30 +1,28 @@
 //program to find whether given year is leap year or not
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool leap_year(int n){
-   //Enter your code here
- if (n % 400 == 0)
-        return true;
+int main() {
 
+  int year;
+  cout << "Enter a year: ";
+  cin >> year;
 
-    if (n % 100 == 0)
-        return false;
-
-
-    if (n % 4 == 0)
-        return true;
-    return false;
-}
-
-//Driver Code
-int main()
-{
-    int n;
-    cin>>n;
-    if(leap_year(n))
-      cout<<"Leap year";
-    else
-      cout<<"Not a leap year";
-    return 0;
+  // leap year if perfectly divisible by 400
+  if (year % 400 == 0) {
+    cout << year << " is a leap year.";
+  }
+  // not a leap year if divisible by 100, but not divisible by 400
+  else if (year % 100 == 0) {
+    cout << year << " is not a leap year.";
+  }
+  // leap year if not divisible by 100, but divisible by 4
+  else if (year % 4 == 0) {
+    cout << year << "Leap year.";
+  }
+  
+  else {
+    cout << year << "Not a Leap year.";
+  }
+  return 0;
 }
